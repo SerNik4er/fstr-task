4a0971fd6d91ddd328cfd7ad3b67001c6308c08f
SerNik4er <106673431+SerNik4er@users.noreply.github.com>
2023-04-07 18:25:54 +0300

    Add files via upload

--- FSTR/FSTR/urls.py
+++ FSTR/FSTR/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('app.urls')),
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('', SpectacularSwaggerView.as_view(template_name='swagger.html', url_name='schema'), name='swagger')
 ]
